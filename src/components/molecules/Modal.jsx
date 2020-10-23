8e13793b3b6c99f4db82018ddc9b8ec5bc9dbfc8
rogeliosamuel621 <rogeliosamuel621@gmail.com>
2020-10-23 09:27:15 -0600

    [UPDATE] Modal

--- src/components/molecules/Modal.jsx
+++ src/components/molecules/Modal.jsx
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../../styles/molecules/modal.css';
 
+import { Input, Submit } from '../atoms/';
+
 const Modal = () => {
+  const [email, setEmail] = useState('');
+
   return (
     <div id="Modal" className="Modal">
       <div className="Modal-Content">
         <div className="Modal-Content-Close">
           <a href="#">X</a>
         </div>
-        <h1>My modal</h1>
+        <h1>Add a contact</h1>
+        <form>
+          <Input label="Email" type="email" onChange={setEmail} value={email} />
+          <Submit value="Add contact" />
+        </form>
       </div>
     </div>
   );
